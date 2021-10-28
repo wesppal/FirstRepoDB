d8a0422144c0b54ac6837ef13da65eff92aa3167
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-10-28 19:17:25 +0300

    Creating getter, setter, override toString

--- src/by/git/firstproject/entity/User.java
+++ src/by/git/firstproject/entity/User.java
@@ -8,4 +8,53 @@ public class User {
     private int age;
     private String phoneNumber;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return  "id: " + id +
+                ", name: " + name +
+                ", surname: " + surname +
+                ", age: " + age +
+                ", phoneNumber: " + phoneNumber;
+    }
 }
