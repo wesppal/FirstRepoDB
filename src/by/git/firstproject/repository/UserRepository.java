d1a61e70d624d5115361d63ea31c445f708ff186
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-10-28 19:21:24 +0300

    Fill interface

--- src/by/git/firstproject/repository/UserRepository.java
+++ src/by/git/firstproject/repository/UserRepository.java
@@ -1,6 +1,16 @@
 package by.git.firstproject.repository;
 
+import by.git.firstproject.entity.User;
+
 public interface UserRepository {
 
+    User[] getAllUsers ();
 
+    User getUserById (long id);
+
+    User getUserByFullName (String fullName);
+
+    User removeUserById (long id);
+
+    User addUser (User user);
 }
